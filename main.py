ee676d59cd5bbbc69c914a53ceeb0467862cea16
Finn <finn@fsj.xyz>
2022-01-05 11:05:55 -0500

    ✨ initial commit ✨

---
+++ main.py
@@ -0,0 +1,76 @@
+from flask import Flask, json, request
+import numpy as np
+import ast
+
+api = Flask(__name__)
+
+
+cols = [
+    "0",
+    "junk",
+    "junk",
+    "junk",
+    "U",
+    "B",
+    "V",
+    "R",
+    "I",
+    "J",
+    "H",
+    "K",
+    "uprime",
+    "gprime",
+    "rprime",
+    "iprime",
+    "zprime",
+    "J",
+    "H",
+    "Ks",
+    "junk",
+]
+
+
+def find_data_in_files(
+    age: float, metallicity: float, filters: list[str]
+) -> list[float]:
+
+    # attempt to retrieve data from files
+    try:
+        data = np.load(f"./iso-npy-data/Girardi_{age:.2f}_{metallicity:.2f}.npy")
+    except FileNotFoundError:
+        return {"error": "Requested data not found"}
+    # format data
+    try:
+
+        def get_col(number: int):
+            return data[:, cols.index(filters[number])]
+
+        r_data = list(
+            zip([round(a - b, 4) for a, b in zip(get_col(0), get_col(1))], get_col(2))
+        )
+
+        # r_data = list(zip(*[data[:, cols.index(i)] for i in filters]))
+    except ValueError:
+        return {"error": "Requested filter not found"}
+    # return
+    return r_data
+
+
+@api.route("/data", methods=["GET"])
+def get_data():
+    try:
+        age = float(request.args.get("age"))
+        metallicity = float(request.args.get("metallicity"))
+        filters = ast.literal_eval(request.args.get("filters"))
+        print(filters)
+    except ValueError:
+        return json.dumps({"error": "Input invalid type"})
+    return json.dumps(find_data_in_files(age, metallicity, filters))
+
+
+def main():
+    api.run()
+
+
+if __name__ == "__main__":
+    main()
