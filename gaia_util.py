ea59cc6af464cc0ae356949681f43e266c6543c3
Reed Fu <rfugithub@outlook.com>
2022-03-14 13:27:54 -0400

    reintroduce radius searching

--- gaia_util.py
+++ gaia_util.py
@@ -1,22 +1,58 @@
 from os import error
 import sqlite3
 
-from numpy import mat
+
+from numpy import arcsin, cos, deg2rad, rad2deg, sin
 from kdtree import Star, Star_tree
 import math
 
 
 def gaia_get_data(range):
     try:
-        dec_min = float(range['mindec'])
-        dec_max = float(range['maxdec'])
-        ra_min = float(range['minra'])
-        ra_max = float(range['maxra'])
+        dec = float(range['dec'])
+        ra = float(range['ra'])
+        r = float(range['r'])
     except:
         return {"error": "Input invalid type"}
+    if not 0 <= ra < 360:
+        return {'error': 'Expected RA in the range [0, 360)'}
+    if not -90 <= dec < 90:
+        return {'error': 'Expected Dec in the range [-90, +90]'}
+    if not 0 < r < 90:
+        return {'error': 'Expected query radius in the range (0, 90)'}
 
-    where = 'ra >= ? and ra <= ? and dec >= ? and dec <= ?'
-    args = (ra_min, ra_max, dec_min, dec_max)
+    # Compute the RA/Dec query ranges; handle poles and RA=0/360 wrap
+    dec_min, dec_max = dec - r, dec + r
+    if dec_min < -90:
+        # South Pole in FOV, use the whole RA range
+        where = 'dec <= ?'
+        args = (dec_max,)
+    elif dec_max > 90:
+        # North Pole in FOV, use the whole RA range
+        where = 'dec >= ?'
+        args = (dec_min,)
+    else:
+        # See http://janmatuschek.de/LatitudeLongitudeBoundingCoordinates
+        dra = rad2deg(arcsin(sin(deg2rad(r))/cos(deg2rad(dec))))
+        ra_min, ra_max = ra - dra, ra + dra
+        if ra_max >= ra_min + 360:
+            # RA spans the whole 360 range
+            where = 'dec >= ? and dec <= ?'
+            args = (dec_min, dec_max)
+        elif ra_min < 0:
+            # RA range encloses RA=0 => two separate RA ranges:
+            # ra_min + 360 <= ra <= 360 and 0 <= ra <= ra_max
+            where = '(ra >= ? or ra <= ?) and dec >= ? and dec <= ?'
+            args = (ra_min + 360, ra_max, dec_min, dec_max)
+        elif ra_max > 360:
+            # RA range encloses RA=360 => two separate RA ranges:
+            # ra_min <= ra <= 360 and 0 <= ra <= ra_max - 360
+            where = '(ra >= ? or ra <= ?) and dec >= ? and dec <= ?'
+            args = (ra_min, ra_max - 360, dec_min, dec_max)
+        else:
+            # RA range fully within [0, 360)
+            where = 'ra >= ? and ra <= ? and dec >= ? and dec <= ?'
+            args = (ra_min, ra_max, dec_min, dec_max)
     sqlite_filename = 'gaia_clusters.sqlite'
     conn = sqlite3.connect(sqlite_filename)
     try:
@@ -26,7 +62,11 @@ def gaia_get_data(range):
         # formula, which is more accurate for small distances
         cur = conn.cursor()
         sources = cur.execute(
-            'select * from (select * from gedr3dis where ' + where + ')', args).fetchall()
+            'select * from (select * from gedr3dis where ' + where +
+            ') where asin(sqrt(pow(sin(radians(dec - ?)/2), 2) + '
+            'pow(sin(radians(ra - ?)/2), 2)*cos(radians(dec))*?)) <= ?',
+            args + (dec, ra, cos(deg2rad(dec)), deg2rad(r)/2)
+        ).fetchall()
     except error as e:
         print(e)
     finally:
@@ -58,4 +98,7 @@ def gaia_match(photometry, range):
 
 
 # print(gaia_match(
-#     {'id': "src10", 'ra': 9.6, 'dec': -72}, {'minra': 0, 'maxra': 20, 'mindec': -90, 'maxdec': -50}))
+    # [{'id': "src10", 'ra': 9.6, 'dec': -72}], {'minra': 0, 'maxra': 20, 'mindec': -90, 'maxdec': -50}))
+
+print(
+    len(gaia_get_data({'minra': 0, 'maxra': 290, 'mindec': 3, 'maxdec': 9})))
